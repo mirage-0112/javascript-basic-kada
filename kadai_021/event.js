4899821662066d60822f0b7c28983b9b8168f359
mirage-0112 <yorekumoho@gmail.com>
2025-02-05 23:43:40 +0900

    21章課題
    非同期処理を使用して、2秒後に文章を書き換えよう

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+
+const text = document.getElementById('text');
+
+setTimeout(('click',  () => {
+  btn.addEventListener
+    text.textContent = 'ボタンをクリックしました';
+  }), 2000);
+
